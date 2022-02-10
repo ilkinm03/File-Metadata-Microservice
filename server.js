11f8f53c2424e06b8255969dbb3e735b180d9489
nickLee <ilkinm2003@gmail.com>
2022-02-10 16:11:28 +0400

    Complete project

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+   res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+   res.json({
+      name: req.file.filename,
+      type: req.file.mimetype,
+      size: req.file.size,
+   });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+   console.log("Your app is listening on port " + port);
+});
